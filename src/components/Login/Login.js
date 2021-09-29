1d6b62b4ab06d0d294450cffa6ddfb45b02dd163
unknown <rahmanrakibur6589@gmail.com>
2021-09-30 00:53:16 +0600

    create password based account

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import firebase from 'firebase/compat/app';
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword } from "firebase/auth";
 import firebaseConfig from './FirebaseConfig';
 import { Form, Button, Container } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -16,7 +16,10 @@ const Login = () => {
           userLogin: false,
           userName: '',
           userEmail: '',
-          userPhoto: ''
+          userPhoto: '',
+          userPassword: '',
+          userError: '',
+          userSuccess: false
      })
 
      const GoogleSignIn = () => {
@@ -34,18 +37,58 @@ const Login = () => {
                })
      }
 
+     const handleBlur = (e) => {
+          let isFieldValid = true;
+          if (e.target.name === 'email') {
+               isFieldValid = /\S+@\S+\.\S+/.test(e.target.value);
+          }
+          if (e.target.name === 'password') {
+               const passMinLength = e.target.value.length >= 6;
+               const passContainNumber = /\d{1}/.test(e.target.value);
+               isFieldValid = passMinLength && passContainNumber;
+          }
+          if (isFieldValid) {
+               const userInfo = { ...user };
+               userInfo[e.target.name] = e.target.value;
+               setUser(userInfo);
+          }
+     }
+
+     const handleFormSubmit = (e) => {
+          if (user.email && user.password) {
+               const auth = getAuth();
+               createUserWithEmailAndPassword(auth, user.email, user.password)
+                    .then((res) => {
+                         const userInfo = { ...user };
+                         userInfo.userSuccess = true;
+                         userInfo.userError = '';
+                         setUser(userInfo);
+                    })
+                    .catch((err) => {
+                         const userInfo = { ...user };
+                         userInfo.userSuccess = false;
+                         userInfo.userError = err.message;
+                         setUser(userInfo);
+                    })
+          }
+          e.preventDefault();
+     }
+
      return (
           <div style={{ marginTop: "5%" }}>
-               <Container >
-                    <Form style={{ color: 'white' }}>
+               <Container>
+                    <Form style={{ color: 'white' }} onSubmit={handleFormSubmit}>
                          <Form.Label>Name</Form.Label>
-                         <Form.Control type="text" placeholder="Enter Your Name" />
+                         <Form.Control type="text" name='name' required placeholder="Enter Your Name" onBlur={handleBlur} />
                          <Form.Label>Email</Form.Label>
-                         <Form.Control type="email" placeholder="Enter Your Email" />
+                         <Form.Control type="email" name='email' required placeholder="Enter Your Email" onBlur={handleBlur} />
                          <Form.Label>Password</Form.Label>
-                         <Form.Control type="password" placeholder="Enter Your Password" />
-                         <Button variant='secondary' style={{ marginTop: "10px" }}>Submit</Button>
+                         <Form.Control type="password" name='password' required placeholder="Enter Your Password" onBlur={handleBlur} />
+                         <Button variant='secondary' type='submit' style={{ marginTop: "10px" }}>Submit</Button>
                     </Form>
+                    {
+                         user.userSuccess ? <p style={{ color: 'green', textAlign: 'center' }}>"Succsessfully Created!"</p> : <p style={{ color: 'red', textAlign: 'center' }}>{user.userError}</p>
+                    }
                </Container>
                <p style={{ textAlign: 'center', color: 'white' }}>OR</p>
                <div className='d-flex justify-content-center align-items-center'>
