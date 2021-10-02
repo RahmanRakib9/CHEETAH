e9bb6ca69ccb76ca97541f0c4781387786a8ae3d
unknown <rahmanrakibur6589@gmail.com>
2021-10-02 13:28:39 +0600

    updated

--- src/components/Login/FirebaseConfig.js
+++ src/components/Login/FirebaseConfig.js
@@ -5,5 +5,6 @@ const firebaseConfig = {
      storageBucket: "ukulele-shop-ca741.appspot.com",
      messagingSenderId: "707260749266",
      appId: "1:707260749266:web:a9d20456455490b189240b"
-};
+   };
 export default firebaseConfig;
+
